83e9c35a3af8a8906f1d3ab788c1ebea71c0bfe3
officialcactar <officialcactar@gmail.com>
2021-05-15 02:01:34 +0000

    setup inputs

--- Encrypt/encrypt.c
+++ Encrypt/encrypt.c
@@ -1,9 +1,19 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
 int main(int argc, char *argv[]){
 	if (argc != 3){
 		printf("Usage: encrypt {file} {key}\n");
 		return 1;
 	}
-	printf("%s\n", argv[0]);
+	
+	char *input_file = argv[1];
+	char *output_file = malloc(strlen(input_file+4));
+	char *key = argv[2];
+
+	strcpy(output_file, input_file);
+	strcat(output_file, ".enc");
+
+	printf("%s, %s, %s\n", input_file, output_file, key);
 }
